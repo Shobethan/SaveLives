0408d39c604ee1c44f12d9cad970002a9e338682
Shobethan <mshobethan@gmail.com>
2018-06-16 11:34:52 +0530

    Needs Page - Retrieve and show need list added

--- src/pages/needs/needs.ts
+++ src/pages/needs/needs.ts
@@ -1,8 +1,8 @@
 // imports necessary packages for the needs page
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 import { Needs } from '../../models/needs';
 
 import { NeedsCreatePage } from "../needs-create/needs-create";
@@ -15,16 +15,59 @@ import { NeedsCreatePage } from "../needs-create/needs-create";
 
 export class NeedsPage {
 
-  need = {} as Needs;
-  needRef$: AngularFireList<Needs>;
-
-  createdby: string;
+  profileData: any;
+  needEmergencyRef$: AngularFireList<Needs[]>;
+  needEmergencyData: any;
+  needNearbyRef$: AngularFireList<Needs[]>;
+  needNearbyData: any;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
+    private afDatabase: AngularFireDatabase,
     private afAuth: AngularFireAuth,
-    private afDatabase: AngularFireDatabase) {
+    private loadCtrl: LoadingController,
+    private toastCtrl: ToastController) {
+  }
+
+  async ionViewWillEnter() {
+
+    // show the loader
+    var loader = this.loadCtrl.create({
+      spinner: "bubbles",
+      content: "Please wait..."
+    });
+    loader.present();
+
+    try {
+
+      // try to get emergency needs list from firebase database
+      this.needEmergencyRef$ = this.afDatabase.list<Needs[]>('Needs', ref => ref.orderByChild('needbefore').limitToFirst(4));
+      this.needEmergencyData = this.needEmergencyRef$.valueChanges().take(1);
+
+      // try to get nearby needs list from firebase database
+      var userId = this.afAuth.auth.currentUser.uid;
+      // await this.afDatabase.object<string>(`Profile/${this.userId}/district`).valueChanges().take(1).subscribe(District => this.userCurDistrict = District);
+      await this.afDatabase.object<string>(`Profile/${userId}/district`).valueChanges().take(1).subscribe(District => {
+        this.needNearbyRef$ = this.afDatabase.list<Needs[]>('Needs', ref => ref.orderByChild("district").equalTo(District).limitToFirst(4));
+        this.needNearbyData = this.needNearbyRef$.valueChanges().take(1);
+      });
+      loader.dismiss();
+    }
+
+    // catch and show errors via toast message only if any errors occur
+    catch (e) {
+      loader.dismiss();
+      console.log(e);
+
+      let toast = this.toastCtrl.create({
+        message: e,
+        duration: 5000,
+        position: "top",
+        cssClass: "error_toast"
+      });
+      toast.present();
+    }
   }
 
   // push needs create page on the top of this page
