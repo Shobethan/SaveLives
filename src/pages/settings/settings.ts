87fdb3e2d32837b0e02b86cd88b626ec4d75ee8c
Shobethan <mshobethan@gmail.com>
2018-06-19 23:29:58 +0530

    Interim Version

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,8 @@
 // imports necessary packages for the settings page
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App, Nav } from 'ionic-angular';
+import { AngularFireAuth} from 'angularfire2/auth';
+import { WelcomePage } from '../welcome/welcome';
 
 @IonicPage()
 @Component({
@@ -12,6 +14,14 @@ export class SettingsPage {
 
   constructor(
     public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public navParams: NavParams, 
+    private afAuth: AngularFireAuth,
+    public app: App) {
+  }
+
+  async logout() {
+  await this.afAuth.auth.signOut();
+  var nav = this.app.getRootNav();
+  nav.setRoot(WelcomePage);
   }
 }
